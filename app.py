b5733966bc0947ceb3277decbdbe1dfc7b4c6bdf
raspberry <sanddalggi2025@gmail.com>
2025-06-06 06:19:38 +0000

    hahaha

--- app.py
+++ app.py
@@ -240,4 +240,4 @@ def logs():
 # ------------------------- 서버 실행 -------------------------
 if __name__ == '__main__':
     threading.Thread(target=generate_qr_loop, daemon=True).start()
-    socketio.run(app, host="127.0.0.1", port=5000, debug=True, use_reloader=False)
+    socketio.run(app, host="0.0.0.0", port=5000, debug=True, use_reloader=False)
